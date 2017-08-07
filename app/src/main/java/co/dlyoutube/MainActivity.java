603510f3f92372ae32711405523853f4babc5121
Josh Kei <joshkei2001@gmail.com>
2017-08-07 17:55:03 +0800

    20170807

--- app/src/main/java/co/dlyoutube/MainActivity.java
+++ app/src/main/java/co/dlyoutube/MainActivity.java
@@ -1,6 +1,5 @@
 package co.dlyoutube;
 
-import android.accounts.NetworkErrorException;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,19 +7,26 @@ import android.content.Intent;
 import android.widget.EditText;
 import android.view.View;
 import android.os.Environment;
-import android.content.Context;
-import java.io.ByteArrayOutputStream;
+import android.os.AsyncTask;
+
 import java.io.InputStream;
-import java.io.IOException;
+import java.io.BufferedInputStream;
+import java.io.OutputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.net.URLDecoder;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
 
 //testing
 import android.widget.TextView;
+import android.widget.Toast;
 import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +35,8 @@ public class MainActivity extends AppCompatActivity {
     private final String LONG_DOMAIN = "m.youtube.com";
     private final String VIDEO_ORG_STRING = "watch?v=";
     private final String VIDEO_INFO_STRING = "get_video_info?&video_id=";
+    private final String VIDEO_INFO_PATH = "video.info";
+    private final String VIDEO_FILE_PATH = "temp_video";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,78 +49,64 @@ public class MainActivity extends AppCompatActivity {
 
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             if ("text/plain".equals(type)) {
-                handleSendText(intent); // Handle text being sent
+                String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
+                if (sharedText != null && !sharedText.equals("")) {
+                    String videoInfoUrl = changeURL(sharedText);
+
+                    if (videoInfoUrl != null && !videoInfoUrl.equals("")) {
+                        String videoInfoPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/" + VIDEO_INFO_PATH;
+
+                        try {
+                            downloadFile(videoInfoUrl, videoInfoPath, true);
+                        }
+                        catch (IOException ex) {
+
+                        }
+
+                        try {
+                            byte[] videoInfoFile = readFile(videoInfoPath);
+                        }
+                        catch (IOException ex) {
+
+                        }
+
+                        //String videoInfoText = decodeInfo(videoInfo.toString());
+                        //videoInfoText = decodeInfo(videoInfoText);
+                    }
+                }
             }
         }
     }
 
     //testing
     public void testOnClick(View v) {
-        FileOutputStream outputStream = null;
-
         try {
-            byte[] file = downloadFile("https://developer.android.com/reference/android/view/View.OnClickListener.html");
-            String fileName = "test.html";
-
-            outputStream = openFileOutput(fileName, Context.MODE_PRIVATE);
-            outputStream.write(file);
-
-            TextView txtTest = (TextView) findViewById(R.id.test);
-            txtTest.setText(new String(file, StandardCharsets.UTF_8));
-        }
-        catch (Exception e) {
+            AsyncFileDownloader downloader = new AsyncFileDownloader();
+            downloader.execute("https://developer.android.com/reference/android/os/AsyncTask.html", Environment.getExternalStorageDirectory().getAbsolutePath() + "/Download/" + VIDEO_INFO_PATH);
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-        finally {
-            try {
-                outputStream.close();
-            }
-            catch (Exception ex) {
-            }
         }
     }
 
-    private void handleSendText(Intent intent) {
-        String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
-        if (sharedText != null && !sharedText.equals("")) {
-
-            String videoUrl = "";
-            byte[] videoInfo = null;
-            String videoInfoText = "";
-
-            //change URL
-            try {
-                videoUrl = changeURL(sharedText);
-            } catch (IndexOutOfBoundsException ex) {
-                //URL format problem
-            }
-
-            EditText txtUrl = (EditText) findViewById(R.id.url);
-            txtUrl.setText(videoUrl);
-
-            //download video info
-            if (!videoUrl.equals("")) {
-                try {
-                    videoInfo = downloadFile(videoUrl);
-                } catch (IOException e) {
-                    //video info cannot be downloaded
-                }
-            }
-
-            //decode video info
-            if (videoInfo != null) {
-                try {
-                    videoInfoText = decodeInfo(videoInfo.toString());
-                    videoInfoText = decodeInfo(videoInfoText);
-                } catch (UnsupportedEncodingException e) {
-                    //video info cannot be decoded
-                }
-            }
-
-            if (!videoInfoText.equals("")) {
-
-            }
+    private void downloadFile(String url, String path, boolean isOverWritten) throws IOException {
+        if (isOverWritten) {
+            Files.deleteIfExists(Paths.get(path));
         }
+
+        AsyncFileDownloader downloader = new AsyncFileDownloader();
+        downloader.execute(url, path);
+    }
+
+    private byte[] readFile(String path) throws IOException {
+        byte[] file = null;
+
+        Path filePath = Paths.get(path);
+
+        if (Files.exists(filePath)) {
+            Files.readAllBytes(filePath);
+        }
+
+        return file;
     }
 
     private String changeURL(String url) {
@@ -139,40 +133,6 @@ public class MainActivity extends AppCompatActivity {
         return newUrl;
     }
 
-    private byte[] downloadFile(String urlStr) throws IOException {
-        byte[] data = null;
-
-        ByteArrayOutputStream baos = null;
-        InputStream is = null;
-
-        try {
-            baos = new ByteArrayOutputStream();
-
-            URL url = new URL(urlStr);
-            is = url.openStream();
-            byte[] byteChunk = new byte[4096];
-            int n;
-
-            while ((n = is.read(byteChunk)) > 0) {
-                baos.write(byteChunk, 0, n);
-            }
-            data = baos.toByteArray();
-        }
-        catch (IOException e) {
-            throw e;
-        }
-        finally {
-            try {
-                baos.close();
-                is.close();
-            }
-            catch (Exception ex) {
-            }
-        }
-
-        return data;
-    }
-
     private String decodeInfo(String text) throws UnsupportedEncodingException {
         return URLDecoder.decode(text, "UTF-8");
     }
@@ -195,13 +155,73 @@ public class MainActivity extends AppCompatActivity {
     public boolean isExternalStorageReadable() {
         String state = Environment.getExternalStorageState();
         if (Environment.MEDIA_MOUNTED.equals(state) ||
-            Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
             return true;
         }
         return false;
     }
 
-    private void saveFile(byte[] file, String path) {
+    class AsyncFileDownloader extends AsyncTask<String, Integer, Long> {
+        @Override
+        protected Long doInBackground(String... path) {
+            int count;
+            InputStream input = null;
+            OutputStream output = null;
+            long total = 0;
 
+            try {
+                URL u = new URL(path[0]);
+                URLConnection con = u.openConnection();
+                con.connect();
+                // getting file length
+                int lenghtOfFile = con.getContentLength();
+
+                // input stream to read file - with 8k buffer
+                input = new BufferedInputStream(u.openStream());
+
+                // Output stream to write file
+                output = new FileOutputStream(path[1]);
+
+                byte data[] = new byte[1024];
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    //publishProgress(""+(int)((total*100)/lenghtOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+
+                output.flush();
+            } catch (IOException e) {
+                String a = e.toString();
+            } finally {
+                try {
+                    output.close();
+                    input.close();
+                } catch (Exception ex) {
+                }
+            }
+
+            return total;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            super.onProgressUpdate(progress);
+        }
+
+        @Override
+        protected void onPostExecute(Long result) {
+            super.onPostExecute(result);
+
+        }
     }
 }
